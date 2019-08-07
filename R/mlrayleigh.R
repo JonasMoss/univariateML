01111004dff57766cdbacb95772242835443beea
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-07 14:52:15 +0200

    :balloon: Added logLik and tests for the Rayleigh distribution.

--- R/mlrayleigh.R
+++ R/mlrayleigh.R
@@ -5,17 +5,20 @@
 #'
 #' @param x The data from which the estimate is to be computed.
 #' @param na.rm logical. Should missing values be removed?
-#' @return A named numeric vector with maximum likelihood estimates for
-#' \code{location} and \code{scale}.
+#' @return A named numeric vector with the maximum likelihood estimate for
+#'    \code{sigma}.
 #' @export
 
 mlrayleigh = function(x, na.rm = FALSE) {
   if(na.rm) x = x[!is.na(x)] else assertthat::assert_that(!anyNA(x))
-  assertthat::assert_that(min(x) > 0)
 
-  object = c(sigma = sqrt(1/2*mean(x^2)))
+  assertthat::assert_that(min(x) >= 0)
+
+  sigma = sqrt(1/2*mean(x^2))
+  object = c(sigma = sigma)
   class(object) = "univariateML"
   attr(object, "model") = "Rayleigh"
+  attr(object, "logLik") = length(x)*(mean(log(x) - 2*log(sigma) - 1))
   object
 
 }
---
+++ R/test_mlrayleigh.R
@@ -0,0 +1,23 @@
+context("mlrayleigh")
+
+## Data generation.
+set.seed(313)
+small_data = extraDistr::rrayleigh(100, 2)
+tiny_data= extraDistr::rrayleigh(10, 3)
+
+## Finds errors with na and data out of bounds.
+expect_error(mlrayleigh(c(-.1, tiny_data)))
+expect_error(mlrayleigh(c(tiny_data, NA)))
+
+## Checks that na.rm works as intended.
+expect_equal(mlrayleigh(small_data),
+             mlrayleigh(c(small_data, NA), na.rm = TRUE))
+
+## Is the log-likelihood correct?
+est = mlrayleigh(small_data, na.rm = TRUE)
+expect_equal(sum(extraDistr::drayleigh(small_data, est, log = TRUE)),
+             attr(est, "logLik"))
+
+## Check class.
+expect_equal(attr(est, "model"), "Rayleigh")
+expect_equal(class(est), "univariateML")
