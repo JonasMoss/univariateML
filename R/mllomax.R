e1db9da8874e87fd1436c52f635f7abdedd408ec
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-07 22:13:25 +0200

    :construction_worker: Analysis of lomax.

--- R/mllomax.R
+++ R/mllomax.R
@@ -5,7 +5,7 @@
 #' @param x The data from which the estimate is to be computed.
 #' @param na.rm logical. Should missing values be removed?
 #' @param start An optional starting value for the \code{lambda} parameter.
-#'    Defaults to \code{1/median(x)}.
+#'    Defaults to \code{median(x)}.
 #' @param rel.tol Relative accuracy requested.
 #' @param iterlim A positive integer specifying the maximum number of
 #' iterations to be performed before the program is terminated.
@@ -14,12 +14,12 @@
 #' \code{lambda} and \code{kappa}.
 #' @export
 
-mllomax = function(x, na.rm = FALSE, start = 1/stats::median(x),
+mllomax = function(x, na.rm = FALSE, start = stats::median(x),
                    rel.tol = .Machine$double.eps^0.25,
                    iterlim = 100) {
 
   if(na.rm) x = x[!is.na(x)] else assertthat::assert_that(!anyNA(x))
-  assertthat::assert_that(min(x) > 0)
+  assertthat::assert_that(min(x) >= 0)
 
   rel.tol_str = deparse(substitute(rel.tol))
   n = length(x)
@@ -53,20 +53,58 @@ mllomax = function(x, na.rm = FALSE, start = 1/stats::median(x),
   attr(object, "model") = "Lomax"
   object
 
-  # f1 = function(lambda) {
-  #
-  #   sapply(lambda, function(lambda) {
-  #     S = mean(log(1 + lambda*x))
-  #     -log(lambda) + log(S) + S + 1
-  #   })
-  #
-  # }
-  # values = optimize(f = f1, interval = c(0, 1000))
-  # object = c(lambda = values$minimum,
-  #            kappa = 1/mean(log(1 + values$minimum*x)))
-  # class(object) = c("univariateML")
-  # attr(object, "logLik") = -n*values$objective
-  # attr(object, "model") = "Lomax"
-  # object
+  f1 = function(lambda) {
+
+    sapply(lambda, function(lambda) {
+      S = mean(log(1 + lambda*x))
+      -log(lambda) + log(S) + S + 1
+    })
+
+  }
+
+  values = optimize(f = f1, interval = c(0, 1000))
+  object = c(lambda = values$minimum,
+             kappa = 1/mean(log(1 + values$minimum*x)))
+  class(object) = c("univariateML")
+  attr(object, "logLik") = -n*values$objective
+  attr(object, "model") = "Lomax"
+  object
 
 }
+
+
+x = extraDistr::rlomax(10, 1, 7)
+x = extraDistr::rlomax(100, 10, 3)
+lambdas = seq(0, 100, by = 0.01)
+
+
+f1 = function(lambda, x) {
+
+  sapply(lambda, function(lambda) {
+    S = mean(log(1 + lambda*x))
+    -log(lambda) + log(S) + S + 1
+  })
+
+}
+
+
+plot(lambdas, f1(lambdas, tiny_data))
+plot(lambdas, f1(lambdas, small_data))
+plot(lambdas, f1(lambdas, medium_data))
+plot(lambdas, f1(lambdas, large_data))
+
+
+f = Vectorize(function(p, q) mean(extraDistr::dlomax(tiny_data, p, q, log = TRUE)))
+x = seq(0, 1, by = 0.1)
+y = x
+z = outer(x, y, FUN = f)
+contour(x, y, z)
+
+p <- plot_ly(
+  x = x,
+  y = y,
+  z = z,
+  type = "contour"
+)
+
+p
