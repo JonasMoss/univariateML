a5b3ceef93bb3ac82e5f02135431f7fcf79513d8
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-08 11:12:51 +0200

    :banana: Fixed mlcauchy.

--- R/mlcauchy.R
+++ R/mlcauchy.R
@@ -8,27 +8,29 @@
 #' @param x The data from which the estimate is to be computed.
 #' @param na.rm logical. Should missing values be removed?
 #' @return A named numeric vector with maximum likelihood estimates for
-#' \code{mean} and \code{sd}.
+#' \code{location} and \code{scale}.
 #' @export
 
 mlcauchy = function(x, na.rm = FALSE) {
 
   if(na.rm) x = x[!is.na(x)]
-  n = length(x)
 
+  n = length(x)
   m = stats::median(x)
   mad = stats::median(abs(x - m))
 
   start = c(m, mad)
 
   f = function(p) -sum(stats::dcauchy(x, p[1], exp(p[2]), log = TRUE))
-  values = stats::nlm(f = f,
-                      p = start)$estimate
+  values = suppressWarnings(stats::nlm(f = f,
+                      p = start))
 
-  object = c(location = values[1],
-             scale = exp(values[2]))
+  object = c(location = values$estimate[1],
+             scale = exp(values$estimate[2]))
+
   class(object) = "univariateML"
   attr(object, "model") = "Cauchy"
+  attr(object, "logLik") = -values$minimum
   object
 
 }
---
+++ tests/testthat/test_mlcauchy.R
@@ -0,0 +1,51 @@
+context("mlcuachy")
+
+## Data generation.
+set.seed(313)
+tiny_data = stats::rcauchy(10, 1, 7)
+small_data = stats::rcauchy(100, 10, 3)
+medium_data = stats::rcauchy(1000, 1/2, 2)
+large_data = stats::rcauchy(10000, 20, 13)
+
+## Checks if the ML is correct.
+
+mle1 = suppressWarnings(nlm(function(p) {
+  -sum(stats::dcauchy(tiny_data, p[1], p[2], log = TRUE))
+}, p = c(1, 1)))
+
+mle2 = suppressWarnings(nlm(function(p) {
+  -sum(stats::dcauchy(small_data, p[1], p[2], log = TRUE))
+}, p = c(1, 1)))
+
+mle3 = suppressWarnings(nlm(function(p) {
+  -sum(stats::dcauchy(medium_data, p[1], p[2], log = TRUE))
+}, p = c(1, 1)))
+
+mle4 = suppressWarnings(nlm(function(p) {
+  -sum(stats::dcauchy(large_data, p[1], p[2], log = TRUE))
+}, p = c(stats::median(large_data),
+         stats::median(abs(large_data - stats::median(large_data))))))
+
+## Checks estimates.
+expect_equal(mle1$estimate, as.numeric(mlcauchy(tiny_data)), tolerance = 1e-5)
+expect_equal(mle2$estimate, as.numeric(mlcauchy(small_data)), tolerance = 1e-5)
+expect_equal(mle3$estimate, as.numeric(mlcauchy(medium_data)), tolerance = 1e-5)
+expect_equal(mle4$estimate, as.numeric(mlcauchy(large_data)), tolerance = 1e-5)
+
+## Checks logLiks.
+expect_equal(-mle1$minimum, attr(mlcauchy(tiny_data), "logLik"), tolerance = 1e-5)
+expect_equal(-mle2$minimum, attr(mlcauchy(small_data), "logLik"), tolerance = 1e-5)
+expect_equal(-mle3$minimum, attr(mlcauchy(medium_data), "logLik"), tolerance = 1e-5)
+expect_equal(-mle4$minimum, attr(mlcauchy(large_data), "logLik"), tolerance = 1e-5)
+
+## Finds errors with na and data out of bounds.
+expect_error(mlcauchy(c(tiny_data, NA)))
+
+## Checks that na.rm works as intended.
+expect_equal(mlcauchy(small_data),
+             mlcauchy(c(small_data, NA), na.rm = TRUE))
+
+## Check class.
+est = mlcauchy(small_data, na.rm = TRUE)
+expect_equal(attr(est, "model"), "Cauchy")
+expect_equal(class(est), "univariateML")
