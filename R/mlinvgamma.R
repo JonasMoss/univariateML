7f73d7081e5dc81c3f4aa5c316c7a7b8899cb2e8
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-08 12:49:16 +0200

    :banana: Fixed mlinvgamma.

--- R/mlinvgamma.R
+++ R/mlinvgamma.R
@@ -3,20 +3,29 @@
 #' Transforms the data and uses Newton-Raphson to estimate the parameters of the Gamma distribution.
 #'
 #' @param x The data from which the estimate is to be computed.
+#' @param na.rm logical. Should missing values be removed?
 #' @param rel.tol Relative accuracy requested.
 #' @param iterlim A positive integer specifying the maximum number of
 #' iterations to be performed before the program is terminated.
 #' @return A named numeric vector with maximum likelihood estimates for
-#' \code{shape} and \code{rate}.
+#' \code{alpha} and \code{beta}.
 #' @references Choi, S. C, and R. Wette. "Maximum likelihood estimation of the parameters of the gamma distribution and their bias." Technometrics 11.4 (1969): 683-690.
 #' @export
 
 
-mlinvgamma = function(x, rel.tol = .Machine$double.eps^0.25, iterlim = 100) {
+mlinvgamma = function(x, na.rm = FALSE, rel.tol = .Machine$double.eps^0.25, iterlim = 100) {
+  if(na.rm) x = x[!is.na(x)] else assertthat::assert_that(!anyNA(x))
   assertthat::assert_that(min(x) > 0)
-  object = mlgamma(1/x, rel.tol, iterlim)
+
+  object = mlgamma(1/x, na.rm = FALSE, rel.tol = rel.tol, iterlim = iterlim)
+  L = mean(log(x))
+  M = mean(1/x)
   names(object) = c("alpha", "beta")
+  alpha = object[1]
+  beta = object[2]
   class(object) = "univariateML"
-  attr(object, "density") = "stats::dinvgamma"
+  attr(object, "model") = "InvGamma"
+  attr(object, "logLik") = unname(length(x)*(alpha*log(beta) - log(gamma(alpha)) +
+                                -(alpha + 1)*L - beta*M))
   object
 }
---
+++ tests/testthat/test_mlinvgamma.R
@@ -0,0 +1,39 @@
+context("mlinvgamma")
+
+## Data generation.
+set.seed(313)
+small_data = extraDistr::rinvgamma(100, 1, 1)
+tiny_data = extraDistr::rinvgamma(10, 3, 7)
+data3 = c(0, tiny_data)
+
+## Checks if the ML is correct.
+mle1 = nlm(function(p) {
+  -mean(extraDistr::dinvgamma(small_data, p[1], p[2], log = TRUE))
+}, p = c(1,1))$estimate
+
+mle2 = nlm(function(p) {
+  -mean(extraDistr::dinvgamma(tiny_data, p[1], p[2], log = TRUE))
+}, p = c(1,1))$estimate
+
+expect_equal(mle1, as.numeric(mlinvgamma(small_data)), tolerance = 1e-4)
+expect_equal(mle2, as.numeric(mlinvgamma(tiny_data)), tolerance = 1e-4)
+
+## Checks warning with small iterlim.
+expect_warning(mlinvgamma(tiny_data, iterlim = 1))
+
+## Finds errors with na and data out of bounds.
+expect_error(mlinvgamma(c(tiny_data, NA)))
+expect_error(mlinvgamma(c(tiny_data, 0)))
+
+## Checks that na.rm works as intended.
+expect_equal(mlinvgamma(small_data),
+             mlinvgamma(c(small_data, NA), na.rm = TRUE))
+
+## Is the log-likelihood correct?
+est = mlinvgamma(small_data, na.rm = TRUE)
+expect_equal(sum(extraDistr::dinvgamma(small_data, est[1], est[2], log = TRUE)),
+             attr(est, "logLik"))
+
+## Check class.
+expect_equal(attr(est, "model"), "InvGamma")
+expect_equal(class(est), "univariateML")
