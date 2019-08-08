d4b04968f4d4fb71fbc57b8b39cc3b3b41112e97
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-08 15:07:20 +0200

    :banana: Fixed mlgumbel.R

--- R/mlgumbel.R
+++ R/mlgumbel.R
@@ -3,35 +3,38 @@
 #' Uses Newton-Raphson to estimate the parameters of the Gumbel distribution.
 #'
 #' @param x The data from which the estimate is to be computed.
-#' @param scale0 An optional starting value for the \code{scale} parameter.
+#' @param na.rm logical. Should missing values be removed?
+#' @param sigma0 An optional starting value for the \code{sigma} parameter.
 #' @param rel.tol Relative accuracy requested.
 #' @param iterlim A positive integer specifying the maximum number of
 #' iterations to be performed before the program is terminated.
 #'
 #' @return A named numeric vector with maximum likelihood estimates for
-#' \code{shape} and \code{scale}.
+#' \code{shape} and \code{sigma}.
 #' @export
 
-mlgumbel = function(x, scale0 = 1, rel.tol = .Machine$double.eps^0.25,
+mlgumbel = function(x, na.rm = FALSE, sigma0 = 1, rel.tol = .Machine$double.eps^0.25,
                     iterlim = 100) {
+
+  if(na.rm) x = x[!is.na(x)] else assertthat::assert_that(!anyNA(x))
 
   rel.tol_str = deparse(substitute(rel.tol))
   mean_x = mean(x)
 
   for(i in 1:iterlim) {
 
-    A = sum(x*exp(-x/scale0))
-    B = sum(exp(-x/scale0))
-    C = sum(x^2*exp(-x/scale0))
+    A = sum(x*exp(-x/sigma0))
+    B = sum(exp(-x/sigma0))
+    C = sum(x^2*exp(-x/sigma0))
 
-    top = mean_x - scale0 - A/B
-    bottom = -1 - 1/scale0^2*(C/B - (A/B)^2)
+    top = mean_x - sigma0 - A/B
+    bottom = -1 - 1/sigma0^2*(C/B - (A/B)^2)
 
-    scale = scale0 - top/bottom
+    sigma = sigma0 - top/bottom
 
-    if(abs((scale0 - scale)/scale0) < rel.tol) break
+    if(abs((sigma0 - sigma)/sigma0) < rel.tol) break
 
-    scale0 = scale
+    sigma0 = sigma
   }
 
   if(i == iterlim) {
@@ -40,12 +43,15 @@ mlgumbel = function(x, scale0 = 1, rel.tol = .Machine$double.eps^0.25,
                    rel.tol_str, ")."))
   }
 
-  ## Given the shape, the scale is easy to compute.
-  loc = -scale*log(mean(exp(-x/scale)))
+  ## Given the sigma, the mu is easy to compute.
+  mu = -sigma*log(mean(exp(-x/sigma)))
+  S = mean(exp(-(x - mu)/sigma))
 
-  object = c(loc = loc, scale = scale)
+
+  object = c(mu = mu, sigma = sigma)
   class(object) = "univariateML"
-  attr(object, "density") = "Gumbel"
+  attr(object, "model") = "Gumbel"
+  attr(object, "logLik") = -length(x)*(log(sigma) + 1/sigma*(mean_x - mu) + S)
   object
 
 }
--- tests/testthat/test_mlgumbel.R
+++ tests/testthat/test_mlgumbel.R
@@ -1,16 +1,43 @@
 context("mlgumbel")
-set.seed(313)
-data1 = extraDistr::rgumbel(100, 1, 1)
-data2 = extraDistr::rgumbel(10, 3, 7)
 
-mle1 = nlm(function(p) {
-  -mean(extraDistr::dgumbel(data1, p[1], p[2], log = TRUE))
-}, p = c(1,1))$estimate
+## Data generation.
+set.seed(313)
+small_data = extraDistr::rgumbel(100, 1, 1)
+tiny_data = extraDistr::rgumbel(10, 3, 7)
+medium_data = extraDistr::rgumbel(1000, 9, 11)
+
+## Checks if the ML is correct.
+mle1 = suppressWarnings(nlm(function(p) {
+  -sum(extraDistr::dgumbel(small_data, p[1], p[2], log = TRUE))
+}, p = c(1,1)))
 
 mle2 = nlm(function(p) {
-  -mean(extraDistr::dgumbel(data2, p[1], p[2], log = TRUE))
-}, p = c(3,7))$estimate
+  -sum(extraDistr::dgumbel(tiny_data, p[1], p[2], log = TRUE))
+}, p = c(3, 7))
 
-expect_equal(mle1, as.numeric(mlgumbel(data1)), tolerance = 1e-5)
-expect_equal(mle2, as.numeric(mlgumbel(data2)), tolerance = 1e-5)
-expect_warning(mlgumbel(data2, iterlim = 1))
+mle3 = nlm(function(p) {
+  -sum(extraDistr::dgumbel(medium_data, p[1], p[2], log = TRUE))
+}, p = c(9, 11))
+
+expect_equal(mle1$estimate, as.numeric(mlgumbel(small_data)), tolerance = 1e-5)
+expect_equal(mle2$estimate, as.numeric(mlgumbel(tiny_data)), tolerance = 1e-5)
+expect_equal(mle3$estimate, as.numeric(mlgumbel(medium_data)), tolerance = 1e-5)
+
+expect_equal(-mle1$minimum, attr(mlgumbel(small_data), "logLik"), tolerance = 1e-5)
+expect_equal(-mle2$minimum, attr(mlgumbel(tiny_data), "logLik"), tolerance = 1e-5)
+expect_equal(-mle3$minimum, attr(mlgumbel(medium_data), "logLik"), tolerance = 1e-5)
+
+## Checks warning with small iterlim.
+expect_warning(mlgumbel(tiny_data, iterlim = 1))
+
+## Finds errors with na and data out of bounds.
+expect_error(mlgumbel(c(tiny_data, NA)))
+
+## Checks that na.rm works as intended.
+expect_equal(mlgumbel(small_data),
+             mlgumbel(c(small_data, NA), na.rm = TRUE))
+
+est = mlgumbel(tiny_data)
+## Check class.
+expect_equal(attr(est, "model"), "Gumbel")
+expect_equal(class(est), "univariateML")
