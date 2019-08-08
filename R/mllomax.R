c4868bad75bd4a517f3f0e9c64e393b6a5c865c3
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-08 10:53:27 +0200

    :alien: Added Lomax stuff.
    Quick fix of non-convergence. Will have to look at more.

--- R/mllomax.R
+++ R/mllomax.R
@@ -24,6 +24,8 @@ mllomax = function(x, na.rm = FALSE, start = stats::median(x),
   rel.tol_str = deparse(substitute(rel.tol))
   n = length(x)
 
+  # Check if
+
   lambda0 = start
 
   for(i in 1:iterlim) {
@@ -35,14 +37,14 @@ mllomax = function(x, na.rm = FALSE, start = stats::median(x),
     lambda = lambda0 - top/bottom
 
     if(abs((lambda0 - lambda)/lambda0) < rel.tol) break
-
+    if(lambda < 0.00001) stop("The maximum likelihood estimator of the Lomax distribution does not exist. ")
     lambda0 = lambda
   }
 
   if(i == iterlim) {
-    warning(paste0("The iteration limit (iterlim = ", iterlim, ") was reached",
-                   " before the relative tolerance requirement (rel.tol = ",
-                   rel.tol_str, ")."))
+      warning(paste0("The iteration limit (iterlim = ", iterlim, ") was reached",
+                     " before the relative tolerance requirement (rel.tol = ",
+                     rel.tol_str, ")."))
   }
 
   S = mean(log(1 + lambda*x))
@@ -53,58 +55,5 @@ mllomax = function(x, na.rm = FALSE, start = stats::median(x),
   attr(object, "model") = "Lomax"
   object
 
-  f1 = function(lambda) {
-
-    sapply(lambda, function(lambda) {
-      S = mean(log(1 + lambda*x))
-      -log(lambda) + log(S) + S + 1
-    })
-
-  }
-
-  values = optimize(f = f1, interval = c(0, 1000))
-  object = c(lambda = values$minimum,
-             kappa = 1/mean(log(1 + values$minimum*x)))
-  class(object) = c("univariateML")
-  attr(object, "logLik") = -n*values$objective
-  attr(object, "model") = "Lomax"
-  object
-
 }
 
-
-x = extraDistr::rlomax(10, 1, 7)
-x = extraDistr::rlomax(100, 10, 3)
-lambdas = seq(0, 100, by = 0.01)
-
-
-f1 = function(lambda, x) {
-
-  sapply(lambda, function(lambda) {
-    S = mean(log(1 + lambda*x))
-    -log(lambda) + log(S) + S + 1
-  })
-
-}
-
-
-plot(lambdas, f1(lambdas, tiny_data))
-plot(lambdas, f1(lambdas, small_data))
-plot(lambdas, f1(lambdas, medium_data))
-plot(lambdas, f1(lambdas, large_data))
-
-
-f = Vectorize(function(p, q) mean(extraDistr::dlomax(tiny_data, p, q, log = TRUE)))
-x = seq(0, 1, by = 0.1)
-y = x
-z = outer(x, y, FUN = f)
-contour(x, y, z)
-
-p <- plot_ly(
-  x = x,
-  y = y,
-  z = z,
-  type = "contour"
-)
-
-p
--- tests/testthat/test_mllomax.R
+++ tests/testthat/test_mllomax.R
@@ -4,52 +4,43 @@ context("mllomax")
 set.seed(313)
 tiny_data = extraDistr::rlomax(10, 1, 7)
 small_data = extraDistr::rlomax(100, 10, 3)
-medium_data= extraDistr::rlomax(1000, 1/2, 2)
-large_data= extraDistr::rlomax(1000, 20, 13)
+medium_data = extraDistr::rlomax(1000, 1/2, 2)
+large_data = extraDistr::rlomax(10000, 20, 13)
 
 ## Checks if the ML is correct.
-mle1 = suppressWarnings(nlm(function(p) {
-  -mean(extraDistr::dlomax(tiny_data, p[1], p[2], log = TRUE))
-}, p = c(1,1))$estimate)
-
 mle2 = suppressWarnings(nlm(function(p) {
-  -mean(extraDistr::dlomax(small_data, p[1], p[2], log = TRUE))
+  -sum(extraDistr::dlomax(small_data, p[1], p[2], log = TRUE))
 }, p = c(1,1))$estimate)
 
 mle3 = suppressWarnings(nlm(function(p) {
-  -mean(extraDistr::dlomax(medium_data, p[1], p[2], log = TRUE))
+  -sum(extraDistr::dlomax(medium_data, p[1], p[2], log = TRUE))
 }, p = c(1,1))$estimate)
 
 mle4 = suppressWarnings(nlm(function(p) {
-  -mean(extraDistr::dlomax(large_data, p[1], p[2], log = TRUE))
+  -sum(extraDistr::dlomax(large_data, p[1], p[2], log = TRUE))
 }, p = c(1,1))$estimate)
 
-expect_equal(mle1, as.numeric(mllomax(tiny_data)), tolerance = 1e-5)
-expect_equal(mle2, as.numeric(mllomax(small_data)), tolerance = 1e-5)
-expect_equal(mle3, as.numeric(mllomax(medium_data)), tolerance = 1e-5)
-expect_equal(mle4, as.numeric(mllomax(large_data)), tolerance = 1e-5)
+expect_error(mllomax(tiny_data))
+expect_equal(mle2, as.numeric(mllomax(small_data)), tolerance = 1e-3)
+expect_equal(mle3, as.numeric(mllomax(medium_data)), tolerance = 1e-3)
+expect_equal(mle4, as.numeric(mllomax(large_data)), tolerance = 1e-3)
 
-
-## Checks the control parameters.
-expect_equal(mlbeta(tiny_data, type = "gradient"),
-             mlbeta(tiny_data), tolerance = 1e-5)
-expect_equal(mlbeta(small_data, type = "gradient"),
-             mlbeta(small_data, type = "hessian"), tolerance = 1e-5)
+## Checks warning with small iterlim.
+expect_warning(mllomax(small_data, iterlim = 1))
 
 ## Finds errors with na and data out of bounds.
-expect_error(mlbeta(c(0, tiny_data)))
-expect_error(mlbeta(c(tiny_data, 1)))
-expect_error(mlbeta(c(tiny_data, NA)))
+expect_error(mllomax(c(tiny_data, NA)))
+expect_error(mllomax(c(0, tiny_data)))
 
 ## Checks that na.rm works as intended.
-expect_equal(mlbeta(small_data),
-             mlbeta(c(small_data, NA), na.rm = TRUE))
+expect_equal(mllomax(small_data),
+             mllomax(c(small_data, NA), na.rm = TRUE))
 
 ## Is the log-likelihood correct?
-est = mlbeta(small_data, na.rm = TRUE)
-expect_equal(sum(dbeta(small_data, est[1], est[2], log = TRUE)),
+est = mllomax(small_data, na.rm = TRUE)
+expect_equal(sum(extraDistr::dlomax(small_data, est[1], est[2], log = TRUE)),
              attr(est, "logLik"))
 
 ## Check class.
-expect_equal(attr(est, "model"), "Beta")
+expect_equal(attr(est, "model"), "Lomax")
 expect_equal(class(est), "univariateML")
