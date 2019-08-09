6d037e74e22fc785d74f1f2bb06ada427dd63b5b
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-09 10:43:07 +0200

    :curly_loop: Added plot, lines, points.

--- R/generics.R
+++ R/generics.R
@@ -1,11 +1,59 @@
-# plot.univariateML = function(x, y, ...) {
-#
-#   support = attr(x, "support")
-#   dens = eval(parse(text = univariateML_to_density_string(obj, "d")))
-#   quant = eval(parse(text = univariateML_to_density_string(obj, "q")))
-#
-#
-# }
+plot_wrangler = function(x, range, points = FALSE, ...) {
+
+  if(is.null(range)) {
+    if(abs(attr(x, "support")[1]) + abs(attr(x, "support")[2]) < Inf) {
+      limits = attr(x, "support")
+    } else if (abs(attr(x, "support")[1]) == 0 & abs(attr(x, "support")[2]) == Inf) {
+      limits = c(0, qml(0.99, x))
+    } else {
+      limits = qml(c(0.01, 0.99), x)
+    }
+
+    range = seq(limits[1], limits[2], length.out = 1000)
+
+  }
+
+  defaults = list(type = if(points) "p" else "l",
+                  main = paste0(attr(x, "model"), " model"),
+                  ylab = "Density",
+                  xlab = "x",
+                  lwd  = 1)
+
+  args = listmerge(x = defaults,
+                   y = list(...))
+
+  args$x = range
+  args$y = dml(args$x, x)
+  args
+
+}
+
+
+#' @export
+plot.univariateML = function(x, range = NULL, ...) {
+
+  args = plot_wrangler(x, range, points = FALSE, ...)
+  do.call(graphics::plot, args)
+  invisible(x)
+
+}
+
+#' @export
+lines.univariateML = function(x, range = NULL, ...) {
+
+  args = plot_wrangler(x, range, points = FALSE, ...)
+  do.call(graphics::lines, args)
+  invisible(x)
+
+}
+
+#' @export
+points.univariateML = function(x, range = NULL, ...) {
+  args = plot_wrangler(x, range, points = TRUE, ...)
+  do.call(graphics::points, args)
+  invisible(x)
+
+}
 
 #' @export
 logLik.univariateML = function(object, ...) {
--- tests/testthat/test_generics.R
+++ tests/testthat/test_generics.R
@@ -1,7 +1,7 @@
 context("generics")
 
+set.seed(313)
 # Test of logLik function.
-
 x = stats::rweibull(10, 2 ,3)
 obj = mlweibull(x)
 expect_equal(attr(logLik(obj), "nobs"), 10)
@@ -15,3 +15,27 @@ expect_identical(obj, summary(obj))
 
 # Test of coef function.
 expect_equal(stats::setNames(as.numeric(obj), names(obj)), coef(obj))
+
+# Test of plotting functions
+expect_identical(plot(obj), obj)
+expect_identical(points(obj), obj)
+expect_identical(lines(obj), obj)
+
+x = stats::runif(10, 0, 1)
+obj = mlbeta(x)
+expect_identical(plot(obj), obj)
+expect_identical(points(obj), obj)
+expect_identical(lines(obj), obj)
+
+x = extraDistr::rpower(10, 2 ,3)
+obj = mlpower(x)
+expect_identical(plot(obj), obj)
+expect_identical(points(obj), obj)
+expect_identical(lines(obj), obj)
+
+x = stats::rnorm(10, 2 ,3)
+obj = mlnorm(x)
+expect_identical(plot(obj), obj)
+expect_identical(points(obj), obj)
+expect_identical(lines(obj), obj)
+
