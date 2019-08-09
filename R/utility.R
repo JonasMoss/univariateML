acafd47ca9874f89fb86daf4a4ea397aa87965a7
Jonas Moss <jonas.gjertsen@gmail.com>
2019-08-09 10:17:41 +0200

    :wrench: Added listmerge utility.

---
+++ R/utility.R
@@ -0,0 +1,52 @@
+
+#' Merges two lists.
+#'
+#' @keywords internal
+#' @param x A list of default arguments.
+#' @param y A list of supplied arguments
+#' @param type If \code{merge}, the list will be merge with y
+#' having priority; if \code{template}, named the elements of
+#' y not in x will be discarded after merging.
+#' @return A merged list where conflicts are solved in favour
+#' of y. Does not preserve ordering.
+
+# examples \dontrun{
+#     x = list(a = 5,
+#              b = 0,
+#              c = "a",
+#              d = NULL)
+#
+#     y = list(a = 3,
+#              b = 7,
+#              f = NA)
+#
+#    listmerge(x, y, type = "merge")
+#    listmerge(x, y, type = "template")}
+
+listmerge = function(x, y, type = c("merge", "template")) {
+
+  type = match.arg(type)
+
+  if(length(y) == 0) {
+    return(x)
+  }
+
+  ## Keep and not-keep are quite different.
+  if(type == "merge") {
+    matches = match(names(y), names(x))
+    elements_to_discard = matches[!is.na(matches)]
+    if(length(elements_to_discard) == 0)  {
+      combined = c(y, x)
+    } else{
+      combined = c(y, x[-elements_to_discard])
+    }
+    return(combined)
+  }
+
+  if(type == "template") {
+    matches = match(names(x), names(y))
+    x[!is.na(matches)] = y[matches[!is.na(matches)]]
+    return(x)
+  }
+
+}
---
+++ tests/testthat/test_utility.R
@@ -0,0 +1,15 @@
+context("utility")
+
+x = list(a = 5,
+         b = 0,
+         c = "a",
+         d = NULL)
+
+y = list(a = 3,
+         b = 7,
+         f = NA)
+
+expect_equal(listmerge(x, y, type = "merge")$b, 7)
+expect_null(listmerge(x, y, type = "template")$f, NULL)
+expect_equal(listmerge(x, NULL, type = "merge"), x)
+expect_equal(listmerge(x, list(h = "b"), type = "merge")$h, "b")
