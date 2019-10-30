2a689ee6ee8c4033288de812dde1c7802d8caf7b
Jonas Moss <jonas.gjertsen@gmail.com>
2019-10-30 11:05:38 +0100

    :book: Package docs.

---
+++ R/univariateML.R
@@ -0,0 +1,19 @@
+#' univariateML
+#'
+#' An \code{R}-package for fast, easy, and reliable maximum likelihood estimation for a
+#' selection of parameteric univariate densities.
+#'
+#' Data analysis often requieres the estimation of univariate densities. For
+#' almost all univariate densities maximum likelihood estimation is sensible and
+#' gives consistent and efficient estimators. The purpose of this package is to
+#' do maximum for a large number of densities
+#'
+#' \itemize{
+#'   \item It supports maximum likelihood estimation of a large selection of densities.
+#'   \item It supports plently of generics such as \code{plot} and \code{AIC} which aids in data analysis.
+#' }
+#'
+#' Read the vignettes to learn more about univariateML:
+#' \code{browseVignettes(package = "univariateML")}
+#'
+"_PACKAGE"
