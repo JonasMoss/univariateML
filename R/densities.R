4f945c963d19a2e976f0024581278cb1acdcaa51
Jonas Moss <jonas.gjertsen@gmail.com>
2019-11-12 09:35:04 +0100

    Removed superfluous comments.

--- R/densities.R
+++ R/densities.R
@@ -1,9 +1,3 @@
-# densities = unique(c("mlcauchy", "mlgumbel", "mllaplace", "mllogis", "mlnorm",
-#                      "mlbetapr", "mlexp", "mlgamma", "mlinvgamma", "mlinvgauss",
-#                      "mlinvweibull", "mlllogis", "mllnorm", "mllomax", "mlrayleigh",
-#                      "mlweibull", "mllgamma", "mlpareto", "mlbeta", "mlkumar",
-#                      "mllogitnorm", "mlunif", "mlpower"))
-
 densities = list.files("R")
 densities = densities[sapply(densities, function(x) substr(x, 1, 2) == "ml")]
 densities = unname(unlist(sapply(densities, function(x) strsplit(x, ".R"))))
