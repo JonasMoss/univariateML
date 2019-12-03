ecb3f284cd7d67fd70529821b1cb654b7d19a55a
Jonas Moss <jonas.gjertsen@gmail.com>
2019-12-03 11:54:02 +0100

    :hammer: Added test of formals.

--- tests/testthat/test_input_checks.R
+++ tests/testthat/test_input_checks.R
@@ -7,3 +7,10 @@ for (dens in densities) {
   eval(call("expect_error", call(dens, replicate(3, 1:3))))
   eval(call("expect_error", call(dens, NA)))
 }
+
+forms_rhs = c("x","na.rm","...")
+for (dens in densities) {
+  eval(call("context", paste0("Formals: ", dens)))
+  forms_lhs = names(eval(call("formals", dens)))
+  expect_equal(forms_lhs, forms_rhs)
+}
