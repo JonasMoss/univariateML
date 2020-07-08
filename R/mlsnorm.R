940893aac5e7c74b20d14282d2aed0d488c9c82a
tnagler <thomas.nagler@tum.de>
2020-07-08 12:59:04 +0200

    add Skew normal (mlsnorm)

--- R/mlsnorm.R
+++ R/mlsnorm.R
@@ -1,16 +1,16 @@
-#' Student-t distribution maximum likelihood estimation
+#' Skew Normal distribution maximum likelihood estimation
 #'
-#' Joint maximum likelihood estimation as implemented by [fGarch::stdFit].
+#' Joint maximum likelihood estimation as implemented by [fGarch::snormFit].
 #'
 #' For the density function of the Student t distribution see
-#' [dstd][fGarch::std].
+#' [dsnorm][fGarch::snorm].
 #'
 #' @param x a (non-empty) numeric vector of data values.
 #' @param na.rm logical. Should missing values be removed?
 #' @param ... currently affects nothing.
-#' @return `mlstd` returns an object of [class][base::class] `univariateML`.
+#' @return `mlsnorm` returns an object of [class][base::class] `univariateML`.
 #'    This is a named numeric vector with maximum likelihood estimates for
-#'    the parameters `mean`, `sd`, `nu` and the following attributes:
+#'    the parameters `mean`, `sd`, `xi` and the following attributes:
 #'     \item{`model`}{The name of the model.}
 #'     \item{`density`}{The density associated with the estimates.}
 #'     \item{`logLik`}{The loglikelihood at the maximum.}
@@ -18,21 +18,21 @@
 #'     \item{`n`}{The number of observations.}
 #'     \item{`call`}{The call as captured my `match.call`}
 #' @examples
-#' mlstd(precip)
-#' @seealso [dstd][fGarch::std] for the Student-t density.
-#' @references Johnson, N. L., Kotz, S. and Balakrishnan, N. (1995)
-#' Continuous Univariate Distributions, Volume 1, Chapter 13. Wiley, New York.
+#' mlsnorm(precip)
+#' @seealso [dsnorm][fGarch::snorm] for the Student-t density.
+#' @references Fernandez C., Steel M.F.J. (2000); On Bayesian Modelling of Fat
+#'     Tails and Skewness, Preprint.
 #' @export
 
-mlstd <- function(x, na.rm = FALSE, ...) {
+mlsnorm <- function(x, na.rm = FALSE, ...) {
   if (na.rm) x <- x[!is.na(x)] else assertthat::assert_that(!anyNA(x))
   ml_input_checker(x)
 
-  fit <- suppressWarnings(fGarch::stdFit(x))
+  fit <- suppressWarnings(fGarch::snormFit(x))
   object <- fit[["par"]]
   class(object) <- "univariateML"
-  attr(object, "model") <- "Student-t"
-  attr(object, "density") <- "fGarch::dstd"
+  attr(object, "model") <- "Skew Normal"
+  attr(object, "density") <- "fGarch::dsnorm"
   attr(object, "logLik") <- -fit$objective
   attr(object, "support") <- c(-Inf, Inf)
   attr(object, "n") <- length(x)
