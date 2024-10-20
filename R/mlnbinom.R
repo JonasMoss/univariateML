c833e65afc2a501aa6b97f1794f99d685cfe1d01
Jonas Moss <jonas.gjertsen@gmail.com>
2024-10-20 11:11:51 +0200

    :bug: Fix when all observations are 0; add initial MoM estimators.

--- R/mlnbinom.R
+++ R/mlnbinom.R
@@ -6,7 +6,7 @@
 #' @param x a (non-empty) numeric vector of data values.
 #' @param na.rm logical. Should missing values be removed?
 #' @param ... The arguments `size` can be specified to only return the ml of `prob`.
-#'    `rel.tol` is the relative accuracy requested,
+#'    `reltol` is the relative accuracy requested,
 #'    defaults to `.Machine$double.eps^0.25`. `iterlim` is a positive integer
 #'    specifying the maximum number of iterations to be performed before the
 #'    program is terminated (defaults to `100`).
@@ -34,45 +34,45 @@ metadata$mlnbinom <- list(
 
 mlnbinom_ <- \(x, ...) {
   dots <- list(...)
+  reltol <- get_reltol(dots)
+  iterlim <- get_iterlim(dots)
 
   n <- length(x)
   x_bar <- mean(x)
   get_prob <- \(size) size / (x_bar + size)
 
-  if(is.null(dots$size)) {
-    dget_prob <- \(size) x_bar / (size^2 + x_bar * size)
+  x_bar2 <- mean(x^2)
+  p = 1 - (x_bar/(x_bar2-x_bar^2))
+  size = (1-p)*x_bar/p
 
-    f <- \(size) sum(digamma(x + size)) - n*digamma(size) + n * log(get_prob(size))
-    df <- \(size) sum(trigamma(x + size)) - n * trigamma(size) + n * dget_prob(size)
-
-    rel.tol <- if (!is.null(dots$rel.tol)) {
-      dots$rel.tol
+  if (is.null(dots$size)) {
+    if(mean(x) == 0) {
+      warning("All observations are 0; the maximum likelihood estimator is not unique.")
+      size <- 1
     } else {
-      .Machine$double.eps^0.25
-    }
+      dget_prob <- \(size) x_bar / (size^2 + x_bar * size)
+      f <- \(size) sum(digamma(x + size)) - n * digamma(size) + n * log(get_prob(size))
+      df <- \(size) sum(trigamma(x + size)) - n * trigamma(size) + n * dget_prob(size)
 
-    iterlim <- if (!is.null(dots$iterlim)) {
-      dots$iterlim
-    } else {
-      100
-    }
+      # Start at method of moments estimates.
+      x_bar2 <- mean(x^2)
+      p = 1 - (x_bar/(x_bar2-x_bar^2))
+      size0 = (1-p)*x_bar/p
 
-    size0 <- x_bar / 2
+      for (i in seq(iterlim)) {
+        size <- size0 - f(size0) / df(size0)
+        if (abs((size0 - size) / size0) < reltol) break
 
-    for (i in 1:iterlim) {
-      size <- size0 - f(size0) / df(size0)
-      if (abs((size0 - size) / size0) < rel.tol) break
-
-      size0 <- size
+        size0 <- size
+      }
     }
   } else {
     size <- dots$size
   }
 
   prob <- get_prob(size)
-
-  logLik <- sum(lgamma(x + size)) - sum(lfactorial(x)) - n*lgamma(size) +
-    n * size * log(prob) + n*log(1-prob)*x_bar
+  logLik <- sum(lgamma(x + size)) - sum(lfactorial(x)) - n * lgamma(size) +
+    n * size * log(prob) + n * log(1 - prob) * x_bar
 
   list(estimates = c(size, prob), logLik = logLik)
 }
