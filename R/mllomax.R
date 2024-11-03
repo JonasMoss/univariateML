507d48a7e2ebaff74b8bff5ff8539f5b2ae4a21f
Jonas Moss <jonas.gjertsen@gmail.com>
2024-11-03 10:48:38 +0100

    :recycle: Newton-Raphson helper in lomax.

--- R/mllomax.R
+++ R/mllomax.R
@@ -47,16 +47,22 @@ mllomax <- \(x, na.rm = FALSE, ...) {}
 metadata$mllomax <- list(
   "model" = "Lomax",
   "density" = "extraDistr::dlomax",
-  "support" = intervals::Intervals(c(0, Inf), closed = c(TRUE, FALSE)),
+  "support" = intervals::Intervals(c(0, Inf), closed = c(FALSE, FALSE)),
   "names" = c("lambda", "kappa"),
   "default" = c(1, 2)
 )
 
 mllomax_ <- \(x, ...) {
-  dots <- list(...)
-  reltol <- get_reltol(dots)
-  iterlim <- get_iterlim(dots)
+  f_over_df <- \(lambda0) {
+    S <- mean(log(1 + lambda0 * x))
+    S1 <- mean(x / (1 + lambda0 * x))
+    S2 <- -mean(x^2 / (1 + lambda0 * x)^2)
+    f <- 1 / lambda0 - S1 / S - S1
+    df <- -1 / lambda0^2 - S2 / S + (S1 / S)^2 - S2
+    f / df
+  }
 
+  dots <- list(...)
   if (is.null(dots$lambda0)) {
     s <- mean(x^2)
     m <- mean(x)
@@ -66,37 +72,16 @@ mllomax_ <- \(x, ...) {
     lambda0 <- dots$lambda0
   }
 
-  for (i in seq(iterlim)) {
-    S <- mean(log(1 + lambda0 * x))
-    S1 <- mean(x / (1 + lambda0 * x))
-    S2 <- -mean(x^2 / (1 + lambda0 * x)^2)
-    top <- 1 / lambda0 - S1 / S - S1
-    bottom <- -1 / lambda0^2 - S2 / S + (S1 / S)^2 - S2
-    lambda <- lambda0 - top / bottom
+  lambda <- newton_raphson_1d(f_over_df, lambda0, ...)
 
-    if (abs((lambda0 - lambda) / lambda0) < reltol) break
+  s <- \(lambda) mean(log(1 + lambda * x))
 
-    if (lambda < 0.00001) {
-      stop("The maximum likelihood estimator does not exist. Use `mlexp` to fit an exponential distribution.")
-    }
+  loglik <- \(lambda) sum(extraDistr::dlomax(x, lambda, 1 / s(lambda), log = TRUE))
+  logLik <- loglik(lambda)
 
-    lambda0 <- lambda
-  }
-
-  loglik <- \(lambda, x) {
-    S <- mean(log(1 + lambda * x))
-    -log(lambda) + log(S) + S + 1
-  }
-
-  if (loglik(0.000001, x) < loglik(lambda, x)) {
+  if (loglik(0.000001) > logLik) {
     stop("The maximum likelihood estimator does not exist. Use `mlexp` to fit an exponential distribution.")
   }
 
-  check_iterlim(i, iterlim, reltol)
-
-  S <- mean(log(1 + lambda * x))
-  estimates <- c(lambda, 1 / S)
-  # eariler: n * (log(lambda) - log(S) - S - 1) is numerically inaccurate.
-  logLik <- sum(extraDistr::dlomax(x, lambda, 1 / S, log = TRUE))
-  list(estimates = estimates, logLik = logLik)
+  list(estimates = c(lambda, 1 / s(lambda)), logLik = logLik)
 }
