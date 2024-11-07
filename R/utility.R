3800c0f4fe8db6e1f4cfac5fe5c42cb6211fb7c5
Jonas Moss <jonas.gjertsen@gmail.com>
2024-11-07 16:32:52 +0100

    :sparkles: Inverse digamma function

--- R/utility.R
+++ R/utility.R
@@ -45,10 +45,10 @@ simulate_default <- \(dens, n) {
     substring(strings[2], first = 1, last = 1) <- "r"
     paste0(strings[1], "::", strings[2])
   }
-  params <- metadata[[dens]]$default
-  names(params) <- metadata[[dens]]$names
+  params <- univariateML_metadata[[dens]]$default
+  names(params) <- univariateML_metadata[[dens]]$names
   params["n"] <- n
-  rdist <- to_rdist(metadata[[dens]]$density)
+  rdist <- to_rdist(univariateML_metadata[[dens]]$density)
   rlang::exec(parse(text = rdist)[[1]], !!!as.list(params))
 }
 
@@ -59,40 +59,18 @@ simulate_default <- \(dens, n) {
 #' @param reltol Relative tolerance.
 #' @return Newton--Raphson estimate.
 #' @keywords internal
-newton_raphson_1d <- \(f_over_df, param0, ...) {
+newton_raphson_1d <- \(f_over_df, param0, print = FALSE, ...) {
   dots <- list(...)
-  reltol <- get_reltol(dots)
-  iterlim <- get_iterlim(dots)
+  reltol <- if (!is.null(dots$reltol)) dots$reltol else .Machine$double.eps^0.25
+  iterlim <- if (!is.null(dots$iterlim)) dots$iterlim else 100
 
   for (i in seq(iterlim)) {
     param <- param0 - f_over_df(param0)
-
     if (is.nan(param)) stop("NaN in Newton--Raphson iteration.")
     if (abs((param0 - param) / param0) < reltol) break
-
     param0 <- param
   }
 
-  check_iterlim(i, iterlim, reltol)
-  param
-}
-
-#' @keywords internal
-get_reltol <- \(dots) {
-  if (!is.null(dots$reltol)) {
-    dots$reltol
-  } else {
-    .Machine$double.eps^0.25
-  }
-}
-
-#' @keywords internal
-get_iterlim <- \(dots) {
-  if (!is.null(dots$iterlim)) dots$iterlim else 100
-}
-
-#' @keywords internal
-check_iterlim <- \(i, iterlim, reltol) {
   if (i == iterlim) {
     warning(paste0(
       "The iteration limit (iterlim = ", iterlim, ") was reached",
@@ -100,4 +78,36 @@ check_iterlim <- \(i, iterlim, reltol) {
       reltol, ")."
     ))
   }
+
+  param
+}
+
+
+#' Inverse digamma function
+#'
+#' Calculates the inverse digamma function using Newton--Raphson. Works for
+#' y > -500.
+#'
+#' The number of iterations are few, 1 for most input values, especially those
+#' that are large. The starting value is the lower bound found by Batir (2017).
+#'
+#' @param y Values to invert.
+#' @references Batir, N. (2017). Inequalities for the inverses of the polygamma functions. In arXiv [math.CA]. arXiv. http://arxiv.org/abs/1705.06547
+
+inverse_digamma <- \(y) {
+  if (y < -500) {
+    stop("y must be greater than -500")
+  }
+
+  param0 <- 1/log(1+exp(-y))
+  reltol <- .Machine$double.eps^0.25
+
+  for (i in seq(5)) {
+      param <- param0 + (y - Rfast::Digamma(param0)) / Rfast::Trigamma(param0)
+      if (abs((param0 - param) / param0) < reltol) break
+      param0 <- param
+  }
+
+  param0
+
 }
